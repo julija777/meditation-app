7692e41f6c1f62dc69bedb3726cd8cef0a633c1c
Julija Stepanova (VilŠkerste) <s.julija@me.com>
2025-02-19 23:47:33 +0000

    Revert "origin working branch"
    This reverts commit ff9dad023dd53d41e7418925bf107d9b45f73a35.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,6 +39,7 @@ const Page = () => {
                 }}
             />
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             <Tabs.Screen
                 name="chat"
@@ -50,6 +51,17 @@ const Page = () => {
                 }}
             />
 >>>>>>> 47bb661 (adding chat icon)
+=======
+            <Tabs.Screen
+                name="components"
+                options={{
+                    tabBarLabel: "Components",
+                    tabBarIcon: ({ color }) => (
+                        <Entypo name="map" size={24} color={color} />
+                    ),
+                }}
+            />
+>>>>>>> parent of ff9dad0 (origin working branch)
         </Tabs>
     );
 };
---
+++ app/(tabs)/components/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, Button } from 'react-native';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+
+import Header from '@/components/Header/Header';
+import { router } from 'expo-router';
+import AFFIRMATION_GALLERY from '@/constants/affirmation-gallary';
+
+
+const Components = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  const userName = 'Julia';
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Welcome to the Affirmations Chat! How can I inspire you today?',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Affirmation Bot',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((newMessages: IMessage[] = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+    
+    setTimeout(() => {
+      const randomCategory = AFFIRMATION_GALLERY[Math.floor(Math.random() * AFFIRMATION_GALLERY.length)];
+      const randomAffirmation = randomCategory.data[Math.floor(Math.random() * randomCategory.data.length)];
+      
+      const botMessage: IMessage = {
+        _id: Math.round(Math.random() * 1000000),
+        text: randomAffirmation.text,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Affirmation Bot',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+        image: randomAffirmation.image,
+      };
+      setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]));
+    }, 1000);
+  }, []);
+  
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Header 
+        name={userName} 
+        showBackButton={true} 
+        onBackPress={() => router.back()} 
+        variant="dark" 
+      />
+      <GiftedChat
+        messages={messages}
+        onSend={newMessages => onSend(newMessages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      <Button
+        title="Clear Chat"
+        onPress={() => setMessages([])}
+      />
+    </View>
+  );
+};
+
+export default Components;
