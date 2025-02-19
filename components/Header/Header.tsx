86ab796d81b97be5a231152bac5c50a447a0bb58
Julija Stepanova (VilŠkerste) <s.julija@me.com>
2025-02-19 23:47:33 +0000

    Revert "origin working branch"
    This reverts commit ff9dad023dd53d41e7418925bf107d9b45f73a35.

---
+++ components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Pressable } from 'react-native';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
+interface HeaderProps {
+  name: string;
+  showBackButton?: boolean;
+  onBackPress?: () => void;
+  variant: 'light' | 'dark';
+}
+
+const Header: React.FC<HeaderProps> = ({ name, showBackButton = false, onBackPress, variant }) => {
+  const isDarkMode = variant === 'dark';
+
+  return (
+    <View className={`flex-row items-center justify-between p-16 ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
+      {showBackButton && (
+         <Pressable
+         onPress={() => router.back()}
+         className="right-6"
+     >
+         <AntDesign name="leftcircleo" size={50} color={isDarkMode ? 'white' : 'black'} />
+        </Pressable>
+      )}
+      <Text className={`text-2xl font-bold mr-24 ${isDarkMode ? 'text-white' : 'text-black'}`}>
+        Hello {name}, let me inspire you with some affirmations!
+      </Text>
+    </View>
+  );
+};
+
+export default Header;
